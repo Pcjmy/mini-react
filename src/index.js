85cc49946349e2614aedaa1dd877e73b90248677
Pcjmy <1716045191@qq.com>
2024-03-20 22:56:37 +0800

    feat: cross-env

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import './index.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Hello React</div>);
+const element = <div>Hello React</div>;
+root.render(element);
