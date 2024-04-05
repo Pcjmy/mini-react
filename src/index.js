0e88e71fa7223c1a34674886edf88582be5a75d4
Pcjmy <1716045191@qq.com>
2024-04-05 23:00:59 +0800

    feat: React初始化

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import React from 'react';
-import './index.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = <div>Hello React</div>;
-root.render(element);
+// import './index.css';
+// React18写法
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const element = <div>Hello React</div>;
+// root.render(element);
+
+// 旧版本初始化
+ReactDOM.render(<div>Hello React</div>, document.getElementById('root'));
