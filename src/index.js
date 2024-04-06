cda024bc35e0284e1a2428e4c666c81b83f2bece
Pcjmy <1716045191@qq.com>
2024-04-06 22:35:55 +0800

    feat: delete redundant files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from './react';
 import ReactDOM from './react-dom';
-// import './index.css';
-// React18写法
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// const element = <div>Hello React</div>;
-// root.render(element);
 
-// 旧版本初始化
-// ReactDOM.render(<div>Hello React</div>, document.getElementById('root'));
 ReactDOM.render(<div style={{ color: 'red'}}>Hello React</div>, document.getElementById('root'));
 console.log(<div style={{ color: 'red'}}>Hello React</div>);
