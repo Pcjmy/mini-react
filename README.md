6aa4439045752b856dcabc7ccf70807ecb875386
Pcjmy <1716045191@qq.com>
2024-04-06 23:04:31 +0800

    feat: README

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## README
 
 mini-react
+
+### 实现的功能
+
++ createElement
++ render
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ function MyFunctionComponent(props) {
   return <div style={{ color: 'red'}}>Hello React</div>;
 }
 ReactDOM.render(<MyFunctionComponent />, document.getElementById('root'));
-console.log(<div style={{ color: 'red'}}>Hello React</div>);
+console.log(<MyFunctionComponent />);
