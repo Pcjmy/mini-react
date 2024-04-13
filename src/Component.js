d0fd20ae606a443176fca63c6db5bdae091748c8
Pcjmy <1716045191@qq.com>
2024-04-13 22:50:31 +0800

    feat: setState实现过程

--- src/Component.js
+++ src/Component.js
@@ -1,6 +1,17 @@
 export class Component {
   static IS_CLASS_COMPONENT = true;
   constructor(props) {
+    this.state = {};
     this.props = props;
+  }
+  setState(partialState) {
+    // 1.合并属性
+    // 2.重新渲染进行更新
+    this.update()
+  }
+  update() {
+    // 1.获取重新执行render函数后的虚拟DOM 新虚拟DOM
+    // 2.根据新虚拟DOM生成新的真实DOM
+    // 3.将真实DOM挂载到页面上
   }
 }
