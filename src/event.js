9b36edaaad6fb630494853c978eb9f35f41b10dd
Pcjmy <1716045191@qq.com>
2024-04-20 22:00:39 +0800

    feat: dispatchEvent

--- src/event.js
+++ src/event.js
@@ -1,7 +1,21 @@
+import { updaterQueue, flushUpdateQueue } from './Component';
+
 export function addEvent(dom, eventName, bindFunction) {
   dom.attach = dom.attach || {};
   dom.attach[eventName] = bindFunction;
   // 事件合成机制的核心点一：事件绑定到document
   if (document[eventName]) return ;
   document[eventName] = dispatchEvent;
+}
+
+function dispatchEvent(nativeEvent) {
+  updaterQueue.isBatch = true;
+  // 事件合成机制的核心点二：屏蔽浏览器之间的差异
+  let syntheticEvent = createSyntheticEvent(nativeEvent);
+  flushUpdateQueue();
+}
+
+function createSyntheticEvent(nativeEvent) {
+  let syntheticEvent = {};
+  return syntheticEvent;
 }
