0d7fd4f364a440864d8954141dd776c8edc278f6
Pcjmy <1716045191@qq.com>
2024-06-16 16:50:45 +0800

    feat: useState

--- src/index.js
+++ src/index.js
@@ -1,52 +1,14 @@
-import React from './react';
-import ReactDOM from './react-dom';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-// class Greeting extends React.PureComponent {
-//   render() {
-//     console.log('Greeting render')
-//     return <h3>Hello{this.props.name && ','}{this.props.name}</h3>
-//   }
-// }
-
-// function Greeting({ name }) {
-//   console.log('Greeting render');
-//   return <h3>Hello{name && ','}{name}</h3>
-// }
-
-const Greeting = React.memo(function Greeting({ name }) {
-  console.log('Greeting render');
-  return <h3>Hello{name && ','}{name}</h3>
-})
-
-console.log('Greeting', Greeting);
-
-class MyApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: '', address: '' };
+function Counter() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
   }
-  setName = (newName) => {
-    this.setState({ name: newName });
-  }
-  setAddress = (newAddress) => {
-    this.setState({ address: newAddress });
-  }
-  componentDidUpdate() {
-    console.log('MyApp componentDidUpdate');
-  }
-  render() {
-    return <div>
-      <label>
-        Name:
-        <input onInput = {e => this.setName(e.target.value)} />
-      </label>
-      <label>
-        Address:
-        <input onInput = {e => this.setAddress(e.target.value)} />
-      </label>
-      <Greeting name={this.state.name} />
-    </div>
-  }
+  return <button onClick={handleClick}>
+    You pressed me { count } times
+  </button>
 }
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<Counter/>, document.getElementById('root'));
