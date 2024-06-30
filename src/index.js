d919db45b2a005ad6fc91ccb22e455428c588a2f
Pcjmy <1716045191@qq.com>
2024-06-30 12:25:27 +0800

    feat: useEffect

--- src/index.js
+++ src/index.js
@@ -1,24 +1,68 @@
-import React, { useReducer } from './react';
-import ReactDOM from './react-dom';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
-function reducer(state, action) {
-  if (action.type === 'incremented_age') {
-    return {
-      age: state.age + 1
+export function createConnection(serverUrl, roomId) {
+  // A real implementation would actually connect to the server
+  return {
+    connect() {
+      console.log('✅ Connecting to "' + roomId + '" room at ' + serverUrl + '...');
+    },
+    disconnect() {
+      console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl);
     }
-  }
-  throw Error('Unkonw action.');
+  };
 }
 
-function Counter() {
-  const [state, dispatch] = useReducer(reducer, { age: 22 });
 
-  return <div>
-    <button onClick={() => {
-      dispatch({type: 'incremented_age'})
-    }}>Increment age</button>
-    <p>Hello! You are { state.age }</p>
-  </div>
+function ChatRoom({ roomId }) {
+  const [serverUrl, setServerUrl] = useState('https://localhost:1234');
+
+  useEffect(() => {
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect();
+    return () => {
+      connection.disconnect();
+    };
+  }, [roomId, serverUrl]);
+
+  return (
+    <>
+      <label>
+        Server URL:{' '}
+        <input
+          value={serverUrl}
+          onChange={e => setServerUrl(e.target.value)}
+        />
+      </label>
+      <h1>Welcome to the {roomId} room!</h1>
+    </>
+  );
 }
 
-ReactDOM.render(<Counter/>, document.getElementById('root'));
+export default function App() {
+  const [roomId, setRoomId] = useState('general');
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <label>
+        Choose the chat room:{' '}
+        <select
+          value={roomId}
+          onChange={e => setRoomId(e.target.value)}
+        >
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+      <button onClick={() => setShow(!show)}>
+        {show ? 'Close chat' : 'Open chat'}
+      </button>
+      {show && <hr />}
+      {show && <ChatRoom roomId={roomId} />}
+    </>
+  );
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
