a5153dc5bbf2d46bfe8710ab151af1b411fc63ef
Pcjmy <1716045191@qq.com>
2024-09-28 22:43:25 +0800

    feat: useImperativeHandle

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React, { useRef } from './react';
-import ReactDOM from './react-dom';
+import React, { useRef, useImperativeHandle } from 'react';
+import ReactDOM from 'react-dom';
+
+const MyInput = React.forwardRef(function MyInput(props, ref) {
+  const inputRef = useRef(null);
+  useImperativeHandle(ref, () => {
+    return {
+      focus() {
+        inputRef.current.focus();
+      }
+    }
+  })
+  return <input {...props} ref={inputRef} />;
+})
 
 function Form() {
-  const inputRef = useRef(null);
+  const ref = useRef(null);
   function handleClick() {
-    inputRef.current.focus();
+    ref.current.focus();
   }
 
   return <div>
-    <input ref={inputRef} />
+    <MyInput label="Enter your name:" ref={ref} />
     <button onClick={handleClick}>Focus the input</button>
   </div>
 }
