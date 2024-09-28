b6cda66665a0ea27307cfc4a94ad104ebc8b5ea3
Pcjmy <1716045191@qq.com>
2024-09-28 22:28:46 +0800

    feat: 实现useRef

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useRef } from './react';
+import ReactDOM from './react-dom';
 
 function Form() {
   const inputRef = useRef(null);
